f5fe567bbc5fad4e76c3cb11848558a66232e98c
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-05-01 21:27:39 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RWOnly rwOnly=new RWOnly();
+        //'name' has private access in 'com.driver.RWOnly'
+        //Identifier expected
+        //Unexpected token
+        rwOnly.setName("Yashas");
+        System.out.println("Name: "+ rwOnly.getName());
+    }
 }
